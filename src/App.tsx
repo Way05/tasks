aa09e23fea1d1f585368f1f144ed1db826293683
Wei-en Tan <wtan235@gmail.com>
2026-02-06 14:23:50 -0500

    completed tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -9,6 +10,42 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Hello World
+                <h1>HTML & CSS</h1>
+                <Container>
+                    <Row>
+                        <img src="../public/logo512.png" alt="react logo" />
+                        <Col>
+                            <div
+                                style={{
+                                    width: 300,
+                                    height: 300,
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                            <ol>
+                                <li>One</li>
+                                <li>Two</li>
+                                <li>Three</li>
+                            </ol>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: 300,
+                                    height: 300,
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                            <Button
+                                onClick={() => {
+                                    console.log("Hello World!");
+                                }}
+                            >
+                                Log Hello World
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
             </p>
         </div>
     );
